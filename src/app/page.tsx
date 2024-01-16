44d9986a0534f30ef83b4a590dc69da6cfeb9ae6
Jun Kaneda <kanejun@mac.com>
2024-01-16 11:39:38 +1100

    feat: use cms on about me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Image from "next/image";
 import { Work } from "@/components/Work";
-import { listWorksData } from "@/libs/microcms";
+import { listWorksData, getAboutMeData } from "@/libs/microcms";
 
 export default async function Home() {
   const works = await listWorksData();
+  const aboutMe = await getAboutMeData();
+
+  const aboutMeList: { label: string; items: string[] }[] = [
+    { label: "Key Skills", items: aboutMe.skills.map((skill) => skill.item) },
+    {
+      label: "Work Experience",
+      items: aboutMe.workExperience.map((exper) => exper.item),
+    },
+    { label: "Education", items: aboutMe.education.map((edu) => edu.item) },
+    {
+      label: "Certification",
+      items: aboutMe.certification.map((cert) => cert.item),
+    },
+    {
+      label: "Language",
+      items: aboutMe.language.map((lang) => lang.item),
+    },
+  ];
   return (
     <main className="relative">
       <div className="fixed left-0 top-0">
@@ -21,10 +39,8 @@ export default async function Home() {
         <div className="grow">
           <header className="w-screen bg-gradient-to-tr from-black/50 via-transparent to-transparent">
             <div className="mx-auto flex h-60 max-w-5xl flex-col justify-end whitespace-nowrap px-6 py-8 text-white drop-shadow-md md:px-12">
-              <h1 className="z-10 text-5xl font-black">Jun Kaneda</h1>
-              <p className="z-10 pt-1 text-lg font-bold">
-                Web Application Developer
-              </p>
+              <h1 className="z-10 text-5xl font-black">{aboutMe.name}</h1>
+              <p className="z-10 pt-1 text-lg font-bold">{aboutMe.title}</p>
             </div>
           </header>
 
@@ -48,86 +64,25 @@ export default async function Home() {
                 About Me
               </h2>
 
-              <div className="text-sm">
-                <p className="mt-6">
-                  I am a web application developer, proficient in the technical
-                  design and implementation of web applications. I have over
-                  four years of work experience in Japan, focusing on the
-                  design, administration, and development of eCommerce systems
-                  within the retail sector. In addition to my primary role, I
-                  actively pursued freelance and volunteer opportunities to
-                  continually enhance my development skills.
-                </p>
-                <p className="mt-2">
-                  Possessing a level-headed and responsible personality, I am
-                  well-suited for a developer role. My upbringing, education,
-                  and diverse work background have equipped me with
-                  comprehensive knowledge spanning from infrastructure to
-                  front-end development. This broad understanding, coupled with
-                  a high viewpoint and critical thinking, enables me to quickly
-                  adapt to various projects, collaborate effectively with
-                  members in diverse roles and play a pivotal role.
-                </p>
-              </div>
+              <p className="mt-6 text-sm">{aboutMe.introduction}</p>
 
-              <h3 className="pb-2 pt-6 text-xl font-extrabold">Key Skills</h3>
-              <ul className="ml-4 list-outside list-disc text-sm">
-                <li>
-                  Jamstack Web Development (TypeScript, Vue.js, React, Next.js,
-                  Tailwind CSS, Contentful, Stripe, etc..)
-                </li>
-                <li>
-                  Amazon Web Service (Especially Lambda, DynamoDB, S3,
-                  CloudFront, etc..)
-                </li>
-              </ul>
-
-              <h3 className="pb-2 pt-6 text-xl font-extrabold">
-                Work Experience
-              </h3>
-              <ul className="ml-4 list-outside list-disc text-sm">
-                <li>Freelance Programmer (2019-2023)</li>
-                <li>
-                  System Analyst and Application Systems Engineer, Accenture
-                  Japan Ltd, Japan (2019-2023)
-                </li>
-                <li>
-                  Assistant of Programming School for Kids, MIMAMORUME CO.,LTD.
-                  , Japan (2017-2019)
-                </li>
-              </ul>
-
-              <h3 className="pb-2 pt-6 text-xl font-extrabold">Education</h3>
-              <ul className="ml-4 list-outside list-disc text-sm">
-                <li>
-                  Master of Engineering, Computer Networking, Osaka University,
-                  Japan (2017-2019)
-                </li>
-                <li>
-                  Bachelor of Engineering, Computer Science, Osaka University,
-                  Japan (2013-2017)
-                </li>
-              </ul>
-
-              <h3 className="pb-2 pt-6 text-xl font-extrabold">
-                Certification
-              </h3>
-              <ul className="ml-4 list-outside list-disc text-sm">
-                <li>AWS Certified Cloud Practitioner</li>
-                <li>ITIL Foundation</li>
-                <li>Applied Information Technology Engineer (Japan)</li>
-              </ul>
-
-              <h3 className="pb-2 pt-6 text-xl font-extrabold">Language</h3>
-              <ul className="ml-4 list-outside list-disc text-sm">
-                <li>Japanese</li>
-                <li>English</li>
-              </ul>
+              {aboutMeList.map((aboutMeItems, index) => (
+                <div key={index}>
+                  <h3 className="pb-2 pt-6 text-xl font-extrabold">
+                    {aboutMeItems.label}
+                  </h3>
+                  <ul className="ml-4 list-outside list-disc text-sm">
+                    {aboutMeItems.items.map((item, index) => (
+                      <li key={index}>{item}</li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
             </section>
 
             <footer>
               <div className="mx-auto max-w-5xl px-6 pb-16 pt-3 text-center text-xs md:px-12 md:pt-8">
-                © 2023 Jun Kaneda
+                {`© 2023-2024 ${aboutMe.name}`}
               </div>
             </footer>
           </div>
--- src/libs/microcms.ts
+++ src/libs/microcms.ts
@@ -27,11 +27,36 @@ export type WorkType = {
   link: string;
 };
 
+export type ItemsType = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  category: [string];
+  item: string;
+};
+
+export type AboutMeType = {
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  name: string;
+  title: string;
+  introduction: string;
+  skills: ItemsType[];
+  workExperience: ItemsType[];
+  education: ItemsType[];
+  certification: ItemsType[];
+  language: ItemsType[];
+};
+
 export async function listWorksData(): Promise<WorkType[]> {
   const res = await client.get({
     endpoint: "works",
     customRequestInit: {
-      cache: "no-store", // キャッシュを利用せずに常に新しいデータを取得する
+      cache: "no-store",
     },
   });
 
@@ -41,4 +66,20 @@ export async function listWorksData(): Promise<WorkType[]> {
   }
 
   return res.contents as WorkType[];
+}
+
+export async function getAboutMeData(): Promise<AboutMeType> {
+  const res = await client.get({
+    endpoint: "aboutme",
+    customRequestInit: {
+      cache: "no-store",
+    },
+  });
+
+  if (!res) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res as AboutMeType;
 }
